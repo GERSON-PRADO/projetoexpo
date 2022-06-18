a7b225d1a0a218ed6ef08378de49e1d33c37812e
GERSON-PRADO <devpgto@gmail.com>
2022-06-18 16:58:03 +0100

    Criado a pagina Home e Movies

---
+++ src/pages/movies/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native-web';
+
+function Movies() {
+    return (
+        <View>
+            <Text>Tela meus  filmes</Text>
+        </View>
+    )
+}
+export default Movies;
